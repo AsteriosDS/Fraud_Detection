14147f3c106d253c647915101800def7a9ccb4d4
AsteriosDS <117105637+AsteriosDS@users.noreply.github.com>
2023-07-02 16:46:20 +0300

    Update fraud_app.py

--- fraud_app.py
+++ fraud_app.py
@@ -20,10 +20,27 @@ def main():
     # Load test set time series data
     test_data = pd.read_csv(path + "/val_st.csv")
 
-    # Display micrographs of time series data
+    # Display micrographs of time series data in a 5x20 grid
     st.subheader("Micrographs of Time Series")
-    for i, row in test_data.iterrows():
-        st.line_chart(row)
+    num_rows = 5
+    num_cols = 20
+    num_plots = num_rows * num_cols
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=(20, 10))
+    
+    for i, row in enumerate(test_data.iterrows()):
+        if i >= num_plots:
+            break
+        
+        row_index = i // num_cols
+        col_index = i % num_cols
+        ax = axes[row_index, col_index]
+        
+        ax.plot(row[1])
+        ax.set_title(f"Sample {i+1}")
+        ax.axis("off")
+    
+    plt.tight_layout()
+    st.pyplot(fig)
 
     # Classification functionality
     selected_timeseries = st.selectbox("Select a time series to classify", test_data.index)
