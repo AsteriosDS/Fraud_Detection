a4c216d9c2bd08af3f01236c1bbf948bf1bb9608
AsteriosDS <117105637+AsteriosDS@users.noreply.github.com>
2023-07-02 17:55:16 +0300

    Update fraud_app.py

--- fraud_app.py
+++ fraud_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 import numpy as np
 import pandas as pd
 import os
@@ -20,10 +21,6 @@ def main():
     st.title("Fraud Detection Autoencoder")
     st.write("Explore time series data and classify them using the autoencoder model.")
 
-    # # Create a grid layout with two columns
-    # col1, col2 = st.columns((1,1), gap="large")
-
-    # with col1:
     # Display micrographs of time series data in a y times z grid
     st.subheader("Micrographs of Time Series")
     num_rows = 10
@@ -39,43 +36,56 @@ def main():
         col_index = i % num_cols
         ax = axes[row_index, col_index]
         
-        ax.plot(row[1])
-        ax.set_title(f"Sample {i+1}")
-        ax.axis("off")
+        # Create clickable elements for each sample time series
+        component_id = f"time_series_{i}"
+        components.html(f"<p id='{component_id}'>{row[1]}</p>", height=200)
+
+        # Get the selected time series on click
+        selected_timeseries = st.get_query_params().get(component_id)
+        if selected_timeseries:
+            selected_data = test_data.loc[int(selected_timeseries)]
+            reconstructed = model.predict(np.array([selected_data]))[0]
+
+            # Plot original and reconstructed time series together
+            ax.plot(selected_data, label="Original")
+            ax.plot(reconstructed, label="Reconstructed")
+            ax.fill_between(range(len(selected_data)), selected_data, reconstructed, where=(reconstructed >= selected_data), interpolate=True, color='green', alpha=0.5)
+            ax.fill_between(range(len(selected_data)), selected_data, reconstructed, where=(reconstructed < selected_data), interpolate=True, color='red', alpha=0.5)
+            ax.set_title(f"Sample {i+1}")
+            ax.axis("off")
     
     plt.tight_layout(pad=2.5)
     st.pyplot(fig)
 
-    # with col2:
-    # Classification functionality
-    selected_timeseries = st.selectbox("Select a time series to classify", test_data.index)
-    selected_data = test_data.loc[selected_timeseries]
-    reconstructed = model.predict(np.array([selected_data]))[0]
-    reconstruction_error = np.mean(np.abs(selected_data - reconstructed))
-    
-    st.subheader('Original vs Reconstructed Time Series')
-    plt.figure(figsize=(10, 6))
-    plt.plot(selected_data, label="Original")
-    plt.plot(reconstructed, label="Reconstructed")
-    plt.fill_between(range(len(selected_data)), selected_data, reconstructed, where=(reconstructed >= selected_data), interpolate=True, color='green', alpha=0.5)
-    plt.fill_between(range(len(selected_data)), selected_data, reconstructed, where=(reconstructed < selected_data), interpolate=True, color='red', alpha=0.5)
-    plt.xlabel("Features")
-    plt.ylabel("Value")
-    plt.tight_layout(pad=2.5)
-    
-    # Add legends
-    handles, labels = plt.gca().get_legend_handles_labels()
-    handles.extend([
-        plt.Line2D([], [], color='green', alpha=0.5, label='Reconstructed >= Original'),
-        plt.Line2D([], [], color='red', alpha=0.5, label='Reconstructed < Original')
-    ])
-    plt.legend(handles=handles, loc='upper right')
-    st.pyplot(plt)
-    
-    if reconstruction_error > 0.05:
-        st.warning("This time series is classified as fraud.")
-    else:
-        st.success("This time series is not classified as fraud.")
+    if selected_timeseries:
+        # Classification based on reconstruction error
+        selected_data = test_data.loc[int(selected_timeseries)]
+        reconstructed = model.predict(np.array([selected_data]))[0]
+        reconstruction_error = np.mean(np.abs(selected_data - reconstructed))
+        
+        st.subheader('Original vs Reconstructed Time Series')
+        plt.figure(figsize=(10, 6))
+        plt.plot(selected_data, label="Original")
+        plt.plot(reconstructed, label="Reconstructed")
+        plt.fill_between(range(len(selected_data)), selected_data, reconstructed, where=(reconstructed >= selected_data), interpolate=True, color='green', alpha=0.5)
+        plt.fill_between(range(len(selected_data)), selected_data, reconstructed, where=(reconstructed < selected_data), interpolate=True, color='red', alpha=0.5)
+        plt.xlabel("Features")
+        plt.ylabel("Value")
+        plt.tight_layout(pad=2.5)
+
+        # Add legends
+        handles, labels = plt.gca().get_legend_handles_labels()
+        handles.extend([
+            plt.Line2D([], [], color='green', alpha=0.5, label='Reconstructed >= Original'),
+            plt.Line2D([], [], color='red', alpha=0.5, label='Reconstructed < Original')
+        ])
+        plt.legend(handles=handles, loc='upper right')
+        st.pyplot(plt)
+
+        if reconstruction_error > 0.05:
+            st.warning("This time series is classified as fraud.")
+        else:
+            st.success("This time series is not classified as fraud.")
 
 if __name__ == "__main__":
     main()
