e7953a183fd5a976a7dd48168122479d97687a68
AsteriosDS <117105637+AsteriosDS@users.noreply.github.com>
2023-07-02 17:34:05 +0300

    Update fraud_app.py

--- fraud_app.py
+++ fraud_app.py
@@ -21,7 +21,7 @@ def main():
     st.write("Explore time series data and classify them using the autoencoder model.")
 
     # Create a grid layout with two columns
-    col1, col2 = st.columns([0.55,0.45], gap="large")
+    col1, col2 = st.columns((1,1), gap="large")
 
     with col1:
         # Display micrographs of time series data in a y times z grid
